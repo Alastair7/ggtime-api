5536c3f4c07ae1f023e4a4a4cefab51420caea5f
Alastair7 <alexandru.ban.drmer@gmail.com>
2025-02-27 18:21:42 +0100

    GGTIME-1 - Implement healthcheck endpoint

--- cmd/api-server/main.go
+++ cmd/api-server/main.go
@@ -1,8 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"github.com/Alastair7/ggtime-api/internal/api"
+
+	"log"
+	"net/http"
+)
 
 func main() {
-	// Initialize server here
-	fmt.Println("Hello World")
+	http.HandleFunc("/api/healthcheck", api.HealthcheckHandler)
+
+	log.Println("Server is running on: http://localhost:8080")
+	log.Println("You can do a checkhealth with /api/checkhealth")
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
