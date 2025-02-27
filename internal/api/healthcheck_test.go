5e247e428ff987d8f85911f6034476c5189c9245
Alastair7 <alexandru.ban.drmer@gmail.com>
2025-02-27 19:09:50 +0100

    add test for healthcheck endpoint

---
+++ internal/api/healthcheck_test.go
@@ -0,0 +1,20 @@
+package api
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGETHealthcheck(t *testing.T) {
+	t.Run("returns OK if server is running", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodGet, "api/healthcheck", nil)
+		responseRecorder := httptest.NewRecorder()
+
+		HealthcheckHandler(responseRecorder, request)
+
+		if responseRecorder.Code != http.StatusOK {
+			t.Errorf("Expected status 200, got %d", responseRecorder.Code)
+		}
+	})
+}
