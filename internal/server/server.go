7e1c4245425de603bfa99125c23ea32967f73aa3
Alastair7 <alexandru.ban.drmer@gmail.com>
2025-03-10 22:46:16 +0100

    improve logs

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/Alastair7/ggtime-api/internal/api/handlers"
@@ -14,9 +15,9 @@ type ApiServer struct {
 }
 
 func (a *ApiServer) RunServer() {
+	environment := os.Getenv("ENVIRONMENT")
 
 	router := initRouter()
-
 	server := http.Server{
 		Addr:              a.Address,
 		Handler:           router,
@@ -25,7 +26,12 @@ func (a *ApiServer) RunServer() {
 
 	log.Println("You can do a checkhealth with /api/checkhealth")
 	fmt.Println()
-	log.Printf("Server is running on: %s", server.Addr)
+
+	if environment != "production" {
+		log.Printf("Server is running on port: %s", server.Addr)
+	} else {
+		log.Printf("Server is up and running!")
+	}
 
 	log.Fatal(server.ListenAndServe())
 }
