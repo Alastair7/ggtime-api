1583dd7c99ed92f65f663945d6361ef093d3ca6c
Alexandru Ban <alexandru.ban.drmer@gmail.com>
2025-03-17 22:18:39 +0100

    feat: starting point for authorization middleware (#16)
    * Improve server starting logic
    
    * fix environmnent when loading local dotenv
    
    * improve logs
    
    * Add starting point for authorization middleware
    
    * Add unit tests for validator setup
    
    * remove validate claims because there are no claims yet

---
+++ internal/common/jwtValidator.go
@@ -0,0 +1,56 @@
+package common
+
+import (
+	"context"
+	"log"
+
+	jwtmiddleware "github.com/auth0/go-jwt-middleware/v2"
+	"github.com/auth0/go-jwt-middleware/v2/validator"
+)
+
+type ValidatorFactory interface {
+	NewValidator(
+		keyfunc func(context.Context) (interface{}, error),
+		signatureAlgorithm validator.SignatureAlgorithm,
+		issuer string,
+		audience []string,
+	) (*validator.Validator, error)
+}
+
+type DefaultValidatorFactory struct{}
+
+func (d *DefaultValidatorFactory) NewValidator(
+	keyfunc func(context.Context) (interface{}, error),
+	signatureAlgorithm validator.SignatureAlgorithm,
+	issuer string,
+	audience []string,
+) (*validator.Validator, error) {
+
+	return validator.New(keyfunc, signatureAlgorithm, issuer, audience)
+}
+
+type JwtValidator struct {
+	Validator *validator.Validator
+}
+
+func generateKeyFunc(ctx context.Context) (interface{}, error) {
+
+	return []byte("secret"), nil
+}
+
+func (j *JwtValidator) Setup(validatorFactory ValidatorFactory) error {
+	validator, jwtValidatorError := validatorFactory.NewValidator(
+		generateKeyFunc,
+		validator.HS256,
+		"https://ISSUER/",
+		[]string{"AUDIENCE"},
+	)
+
+	if jwtValidatorError != nil {
+		return jwtValidatorError
+	}
+
+	j.Validator = validator
+
+	return nil
+}
---
+++ internal/common/jwtValidator_test.go
@@ -0,0 +1,57 @@
+package common
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/auth0/go-jwt-middleware/v2/validator"
+)
+
+type MockValidatorFactory struct {
+	mockValidator *validator.Validator
+	mockError     error
+}
+
+func (m *MockValidatorFactory) NewValidator(
+	keyfunc func(context.Context) (interface{}, error),
+	signatureAlgorithm validator.SignatureAlgorithm,
+	issuer string,
+	audience []string,
+) (*validator.Validator, error) {
+
+	return m.mockValidator, m.mockError
+}
+
+func TestSetupJwtValidator(t *testing.T) {
+	t.Run("When jwt validator setup fails then return error", func(t *testing.T) {
+		mockFactory := &MockValidatorFactory{
+			mockValidator: nil,
+			mockError:     errors.New("Error setting up the validator")}
+		jwtValidator := &JwtValidator{}
+		errorResult := jwtValidator.Setup(mockFactory)
+
+		if errorResult.Error() != mockFactory.mockError.Error() {
+			t.Fatalf("jwt validator is not empty")
+		}
+
+	})
+
+	t.Run("When jwt validator setup success then return validator", func(t *testing.T) {
+		mockFactory := &MockValidatorFactory{
+			mockValidator: &validator.Validator{},
+			mockError:     nil,
+		}
+
+		jwtValidator := &JwtValidator{}
+		errorResult := jwtValidator.Setup(mockFactory)
+
+		if errorResult != nil {
+			t.Fatalf("Expected no errors but got %v", errorResult.Error())
+		}
+
+		if jwtValidator.Validator == nil {
+			t.Fatalf("Expected validator but got nil")
+		}
+	})
+}
