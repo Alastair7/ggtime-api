75a5699008824dbc4f3c760f9ce30295e28fedf6
Alastair7 <alexandru.ban.drmer@gmail.com>
2025-03-23 20:51:13 +0100

    add igdb auth logic (WIP)

--- internal/third-party/igdb/igdb-api.go
+++ internal/third-party/igdb/igdb-api.go
@@ -1,11 +1,14 @@
 package igdb
 
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
+
 type Authenticator interface {
 	GetAccessToken() (string, error)
-}
-
-type Fetcher interface {
-	Fetch(url string) (interface{}, error)
 }
 
 type IgdbService struct {
@@ -16,15 +19,47 @@ type IgdbService struct {
 func NewIgdbService(auth Authenticator) *IgdbService {
 
 	return &IgdbService{
-		baseUrl: "http://igdb.es",
+		baseUrl: "https://api.igdb.com/v4",
 		auth:    auth,
 	}
 }
 
-type IgdbAuthenticator struct{}
+type IgdbAuthenticator struct {
+	AuthUrl      string
+	ClientId     string
+	ClientSecret string
+	GrantType    string
+}
 
+type IgdbAuthenticatorResponse struct {
+	AccessToken string `json:"access_token"`
+	ExpiresIn   int64  `json:"expires_in"`
+	TokenType   string `json:"token_type"`
+}
+
+// TODO: Test this and change http get with a Post
 func (i *IgdbAuthenticator) GetAccessToken() (string, error) {
-	return "access token", nil
+	igdbResponse, igdbError := http.Get(i.AuthUrl)
+	if igdbError != nil {
+		log.Fatalf("Unable to get access token from IGDB. Details: %v",
+			igdbError)
+	}
+
+	responseBody, readingError := io.ReadAll(igdbResponse.Body)
+	if readingError != nil {
+		log.Fatalf("Unable to read response body. Details: %v",
+			readingError)
+	}
+
+	igdbAuthenticatorResponse := IgdbAuthenticatorResponse{}
+
+	unmarshalError := json.Unmarshal(responseBody, &igdbAuthenticatorResponse)
+	if unmarshalError != nil {
+		log.Fatalf("Error unmarshalling IGDB auth response body. Details %v",
+			unmarshalError)
+	}
+
+	return igdbAuthenticatorResponse.AccessToken, nil
 }
 
 func (ig *IgdbService) Authenticate() (string, error) {
